86e0c771948c4e8e62f91ae51cf872627e75df21
Marcin Kass <marcin.kass10@gmail.com>
2019-03-11 21:05:42 +0100

    small test

---
+++ ASP-Workshop/Models/questionnaire.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_Workshop.Models {
+    public class questionnaire {
+        public string QueryOne { get; set; }
+        public string QueryTwo { get; set; }
+        public string QueryThree { get; set; }
+    }
+}
