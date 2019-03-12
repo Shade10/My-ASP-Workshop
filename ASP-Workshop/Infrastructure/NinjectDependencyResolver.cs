adfa275937003fae8015e8873d14eb959611f5fd
Marcin Kass <marcin.kass10@gmail.com>
2019-03-12 13:37:04 +0100

    added NinjectDependencyResovler for binding interfaces

---
+++ ASP-Workshop/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP_Workshop.Infrastructure.Abstract;
+using ASP_Workshop.Infrastructure.Concrete;
+using Ninject;
+
+namespace ASP_Workshop.Infrastructure {
+    public class NinjectDependencyResolver :IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IAuthProvider>().To<FormsAuthProvider>();
+        }
+    }
+}
